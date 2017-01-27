a0923385f14e3a702acea2589d0f50f3998390f0
Patrick Schmidt <patrick@ealp-net.at>
2017-01-28 00:27:08 +0100

    probes request probe name from server

--- src/at/ealpnet/SensorLogger/probe/SensorLogProbe.java
+++ src/at/ealpnet/SensorLogger/probe/SensorLogProbe.java
@@ -15,6 +15,7 @@ public class SensorLogProbe {
     private InetAddress server;
     private DatagramSocket socket;
 
+    private String sensorName;
 
     public SensorLogProbe(InetAddress server){
         this.server=server;
@@ -30,14 +31,38 @@ public class SensorLogProbe {
                 }
             }
 
-            byte[] data = getSensorData();
+            if(sensorName==null){
+                byte[] sendData=new String("NAMERQST").getBytes();
+                byte[] recvData=new byte[4];
 
-            try {
-                System.out.println("Send: " + new String(data));
-                DatagramPacket packet = new DatagramPacket(data,data.length,server,9999);
-                socket.send(packet);
-            } catch (IOException e) {
-                System.err.println("Error sending packet: " + e);
+                try {
+                    System.out.print("Requesting name from server... ");
+                    DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length,server,9999);
+                    socket.send(sendPacket);
+
+                    DatagramPacket recvPacket= new DatagramPacket(recvData,recvData.length);
+                    socket.receive(recvPacket);
+
+                    sensorName=new String(recvPacket.getData());
+                    sensorName=sensorName.substring(0,recvPacket.getLength());
+
+                    System.out.println(sensorName);
+                } catch (IOException e) {
+                    System.err.println("Error requesting Sensor ID: " + e);
+                }
+            }
+
+            if(sensorName!=null){
+                byte[] data = getSensorData();
+
+                try {
+                    System.out.println("Send: " + new String(data));
+                    DatagramPacket packet = new DatagramPacket(data,data.length,server,9999);
+                    socket.send(packet);
+                } catch (IOException e) {
+                    System.err.println("Error sending packet: " + e);
+                }
+
             }
 
             try {
@@ -47,7 +72,7 @@ public class SensorLogProbe {
     }
 
     private byte[] getSensorData(){
-        String data=String.valueOf(System.currentTimeMillis());
+        String data="[" + sensorName + "](time) " + String.valueOf(System.currentTimeMillis());
         return data.getBytes();
     }
 }
--- src/at/ealpnet/SensorLogger/server/SensorLogServer.java
+++ src/at/ealpnet/SensorLogger/server/SensorLogServer.java
@@ -13,12 +13,15 @@ public class SensorLogServer {
 
     private DatagramSocket serverSocket;
 
+    private int sensorUuid;
+
     public SensorLogServer() throws SocketException {
         serverSocket=new DatagramSocket(9999);
+        sensorUuid=0;
     }
 
     public void run(){
-        byte[] receiveData = new byte[32];
+        byte[] receiveData = new byte[1024];
         DatagramPacket receivePacket;
         String remoteData;
 
@@ -29,7 +32,21 @@ public class SensorLogServer {
             try {
                 serverSocket.receive(receivePacket);
                 remoteData=new String(receivePacket.getData());
-                System.out.println("Received: "+ remoteData);
+                remoteData=remoteData.substring(0,receivePacket.getLength());
+
+                //Init probe with uniqe ID
+                if(remoteData.startsWith("NAMERQST")){
+                    sensorUuid++;
+
+                    String sensorName="P" + sensorUuid;
+                    byte[] sendData = sensorName.getBytes();
+
+                    System.out.println("Name " + sensorUuid + " for " + receivePacket.getSocketAddress().toString().substring(1));
+                    DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length,receivePacket.getSocketAddress());
+                    serverSocket.send(sendPacket);
+                } else {
+                    System.out.println(remoteData);
+                }
             } catch (IOException e) {
                 System.err.println("Error receiving Packet: " + e);
             }
